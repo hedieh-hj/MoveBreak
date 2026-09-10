761bd6102a5d801070a30e8f9d45a8cea392e7dc
hedieh-hj <hediyejamili79@gmail.com>
2026-09-10 17:20:00 +0430

    feat: add a custom MoveBreak app icon

--- Services/NotificationService.cs
+++ Services/NotificationService.cs
@@ -5,6 +5,7 @@ namespace MoveBreak.Services;
 public sealed class NotificationService : IDisposable
 {
     private readonly Forms.NotifyIcon _icon;
+    private readonly Drawing.Icon _appIcon;
     private readonly Forms.ToolStripItem _openItem;
     private readonly Forms.ToolStripItem _pauseItem;
     private readonly Forms.ToolStripItem _exitItem;
@@ -17,18 +18,41 @@ public sealed class NotificationService : IDisposable
         _openItem = menu.Items.Add("", null, (_,_) => ShowRequested?.Invoke());
         _pauseItem = menu.Items.Add("", null, (_,_) => PauseRequested?.Invoke());
         _exitItem = menu.Items.Add("", null, (_,_) => ExitRequested?.Invoke());
-        _icon = new Forms.NotifyIcon { Icon = Drawing.SystemIcons.Information, Text = "MoveBreak", Visible = true, ContextMenuStrip = menu };
+        _appIcon = LoadApplicationIcon();
+        _icon = new Forms.NotifyIcon { Icon = _appIcon, Text = "MoveBreak", Visible = true, ContextMenuStrip = menu };
         _icon.DoubleClick += (_,_) => ShowRequested?.Invoke();
         _icon.MouseClick += (_, e) => { if (e.Button == Forms.MouseButtons.Left) ShowRequested?.Invoke(); };
         _icon.BalloonTipClicked += (_, _) => ShowRequested?.Invoke();
         UpdateLanguage();
         localization.LanguageChanged += UpdateLanguage;
     }
+
+    private static Drawing.Icon LoadApplicationIcon()
+    {
+        var executablePath = Environment.ProcessPath;
+        if (!string.IsNullOrWhiteSpace(executablePath))
+        {
+            var extractedIcon = Drawing.Icon.ExtractAssociatedIcon(executablePath);
+            if (extractedIcon is not null)
+            {
+                return extractedIcon;
+            }
+        }
+
+        return (Drawing.Icon)Drawing.SystemIcons.Application.Clone();
+    }
+
     private void UpdateLanguage() { _openItem.Text=_localization.Text("TrayOpen"); _pauseItem.Text=_localization.Text("TrayPause"); _exitItem.Text=_localization.Text("TrayExit"); }
     public void ShowBreak(string title, string text, bool sound)
     {
         _icon.BalloonTipTitle = title; _icon.BalloonTipText = text; _icon.BalloonTipIcon = Forms.ToolTipIcon.Info;
         _icon.ShowBalloonTip(8000); if (sound) System.Media.SystemSounds.Asterisk.Play();
     }
-    public void Dispose() { _localization.LanguageChanged -= UpdateLanguage; _icon.Visible = false; _icon.Dispose(); }
+    public void Dispose()
+    {
+        _localization.LanguageChanged -= UpdateLanguage;
+        _icon.Visible = false;
+        _icon.Dispose();
+        _appIcon.Dispose();
+    }
 }
